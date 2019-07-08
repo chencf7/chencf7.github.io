9bc96eafb0ad1440f979656b1522b80b7780eb38
chenchaofan <chen_chaofan@hotmail.com>
2019-07-08 15:06:07 +0800

    提交人力资源系统

---
+++ hr-system/precache-manifest.d859daa91e850cd2e71575f81df311a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5d1e8caad91446f9cc9d7500154235e5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c1fdae2143bebb61c052",
+    "url": "/static/css/2.c6a95550.chunk.css"
+  },
+  {
+    "revision": "0b73b4beeea7b630e47d",
+    "url": "/static/css/main.e2dbf123.chunk.css"
+  },
+  {
+    "revision": "c1fdae2143bebb61c052",
+    "url": "/static/js/2.9c639915.chunk.js"
+  },
+  {
+    "revision": "0b73b4beeea7b630e47d",
+    "url": "/static/js/main.0d866e54.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "a5dbd4393ff6a725c7e62b61df7e72f0",
+    "url": "/static/media/login-background.a5dbd439.jpg"
+  }
+]);
