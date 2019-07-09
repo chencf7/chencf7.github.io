fc529c4b31e5ff4c9c31e04b673eedbbb01beadf
chenchaofan <chen_chaofan@hotmail.com>
2019-07-09 23:12:14 +0800

    add background color

--- assets/js/lebroncc.github.io.js
+++ assets/js/lebroncc.github.io.js
@@ -1,12 +1,28 @@
-(function(global, document){
-  global.onload=function(){
-    
+(function(window, document){
+  var currentH=document.body.clientHeight||window.innerHeight||window.screen.availHeight||window.screen.height;
+  var pageObj={
+
+    // 页面初始化方法
+    pageInit: function(){
+      var contentEl=document.querySelector(".fcc-content");
+      contentEl.style.height=currentH+"px";
+      contentEl.style.background="#f1f1f1";
+    }
+  }
+
+  window.onload=function(){
+
+    // 调用初始化方法
+    if(!window.addEventListener){
+      pageObj.pageInit();
+    }
   }
 
 
-  if(global.addEventListener){
+  if(window.addEventListener){
     document.addEventListener('DOMContentLoaded', function(){
       console.log('page DOMContentLoaded');
+      pageObj.pageInit();
     });
   }
 })(window, document);
