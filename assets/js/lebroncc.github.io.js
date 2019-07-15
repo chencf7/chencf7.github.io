04536067523907bc75836dd7aa05f954dd76c98b
chenchaofan <chen_chaofan@hotmail.com>
2019-07-15 12:04:58 +0800

    update

--- assets/js/lebroncc.github.io.js
+++ assets/js/lebroncc.github.io.js
@@ -15,46 +15,72 @@
     // 判断浏览器类型
     var isMobileUserAgent=false;
     var ua = navigator.userAgent.toLowerCase();
-    //mozilla/5.0 (windows nt 6.1; wow64) applewebkit/537.36 (khtml, like gecko) chrome/74.0.3729.169 safari/537.36
-    //mozilla/5.0 (linux; android 6.0; nexus 5 build/mra58n) applewebkit/537.36 (khtml, like gecko) chrome/74.0.3729.169 mobile safari/537.36
-    //mozilla/5.0 (iphone; cpu iphone os 11_0 like mac os x) applewebkit/604.1.38 (khtml, like gecko) version/11.0 mobile/15a372 safari/604.1
     if(ua.indexOf('mobile')>0){
       isMobileUserAgent=true;
     }
 
-    // Slider触摸事件
-    var sliderdom=document.querySelector('.slider-container');
-    // Slider内容需要偏移所属的外部wrapper
-    var sliderwpel = document.querySelector('.slider-wrapper');
-    var sliderwidth=sliderwpel.clientWidth||sliderwpel.offsetWidth;
-    // 判断浏览器种类以及为桌面端还是移动端
+    // Slider图片滑动轮播事件方法
+    ;(function(){
+      var sliderpad=0, 
+        currentslider=0;
+      var startx=0, directx=0;
+      // Slider触摸事件
+      var sliderdom=document.querySelector('.slider-container');
+      // Slider内容需要偏移所属的外部wrapper
+      var sliderwpel = document.querySelector('.slider-wrapper');
+      var sliderwidth=sliderwpel.clientWidth||sliderwpel.offsetWidth;
+      // 判断浏览器种类以及为桌面端还是移动端
 
-    // 设备支持触摸事件
-    //if("ontouchstart" in window){}
-    if(isMobileUserAgent){
-      // 移动端，触摸屏幕开始事件
-      //sliderdom.addEventListener('touchstart', handleStartFunc, false);
-      sliderdom.addEventListener('touchend', handleEndFunc, false);
-      sliderdom.addEventListener('touchmove', handleMoveFunc, false);
-      //function handleStartFunc(){}
-      function handleEndFunc(){
-        
+      // 设备支持触摸事件
+      //if("ontouchstart" in window){}
+      if(isMobileUserAgent){
+        // 移动端，触摸屏幕开始事件
+        sliderdom.addEventListener('touchstart', handleStartFunc, false);
+        sliderdom.addEventListener('touchend', handleEndFunc, false);
+        sliderdom.addEventListener('touchmove', handleMoveFunc, false);
+        function handleStartFunc(event){
+          var points = event.changedTouches[0];
+          startx=points.clientX;
+          directx=points.clientX;
+          console.log(directx);
+        }
+        function handleEndFunc(event){
+          if(currentslider==0){
+            var points = event.changedTouches[0];
+            if(points.clientX>=directx){
+              sliderwpel.style.transform='translate3d(0,0,0)';
+            }
+          }
+          startx=directx=0;
+        }
+        function handleMoveFunc(event){
+          // clientX、clientY 点击位置距离当前body可视区域的x，y坐标
+          var points = event.changedTouches[0];
+          // console.log(points.clientX);
+          if(points.clientX>=directx){
+            //向右滑动
+            sliderpad = points.clientX-startx;
+            if(currentslider==0){
+              if(sliderpad>(sliderwidth/2)){
+                sliderpad=sliderwidth/2;
+              }else
+                sliderpad=points.clientX;
+            }
+          }else{
+            //向左滑动
+            sliderpad = points.clientX-sliderwidth;
+          }
+          sliderwpel.style.transform='translate3d('+sliderpad+'px,0,0)';
+        }
       }
-      function handleMoveFunc(event){
-        // clientX、clientY 点击位置距离当前body可视区域的x，y坐标
-        var points = event.changedTouches[0];
-        // console.log(points);
-        // console.log(points.clientX);
-        sliderwpel.style.transform='translate3d('+(points.clientX-sliderwidth)+'px,0,0)';
+
+      // 支持鼠标事件
+      if("onmousestart" in window){
 
       }
-    }
-
-    // 支持鼠标事件
-    if("onmousestart" in window){
-
-    }
-
+    })();
+    // end-Slider图片滑动轮播事件方法
+    
     // 调用初始化方法，浏览器不支持addEventListener时，直接在onload中执行初始化body的高度
     if(!window.addEventListener){ pageObj.pageInit(); }
   }
